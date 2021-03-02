361a7909ebfbc8b35a37cb66a3bbe9b88e3b43db
Camille Sylla <camillesylla.web@gmail.com>
2021-03-02 08:24:52 +0100

    started responsive animations

--- src/Componant/Pages/Home/Home.js
+++ src/Componant/Pages/Home/Home.js
@@ -10,75 +10,133 @@ import LastProject from "../../Layers/LastProject/LastProject";
 import PSM from "../../Layers/PSM/PSM";
 
 export default function Home() {
+  console.log(window.innerWidth, window.innerHeight);
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
-    gsap.from(".PSM_Img", {
-      scrollTrigger: { 
-          trigger: ".PSM",
-          start: "+=25% +=100%",
-          end: "+=60% +=100%",
-          toggleActions: "restart none none reverse"
-         },
-      x: 400,
-      opacity: 0,
-      duration: 0.5,
-    } )
-    ;
-    gsap.from(".PSM_content", {
-      scrollTrigger: { 
+    if (window.innerWidth < 400 && window.innerHeight){
+      gsap.from(".PSM_content", {
+        scrollTrigger: { 
           trigger: ".PSM",
           start: "+=60% +=100%",
           end: "+=80% +=100%",
           toggleActions: "restart none none reverse"
-         },
-      x: 400,
-      opacity: 0,
-      duration: 0.5,
-    });
-    gsap.to(".scene_container", {
+        },
+        x: 400,
+        opacity: 0,
+        duration: 0.5,
+      });
+      gsap.from(".desc_left, .desc_right, .desc_middle", {
+        scrollTrigger: {
+          trigger: ".desc",
+          markers: true,
+          start: "-25%  50%",
+          toggleActions: "restart none none reverse"
+        },
+        duration: .5,
+        opacity: 0,
+        // stagger: 0.3,
+        y : 100
+      })
+      gsap.to(".scene_container", {
+        scrollTrigger: {
+          trigger: ".hero_banner",
+          start: "+=35% +=30% ",
+          scrub: 0.3,
+          // toggleActions: "none restart none restart",
+        },
+        opacity: 0,
+        y: -500
+      });
+    } else {
+      gsap.from(".PSM_Img", {
+        scrollTrigger: { 
+            trigger: ".PSM",
+            start: "+=25% +=100%",
+            end: "+=60% +=100%",
+            toggleActions: "restart none none reverse"
+          },
+          x: 400,
+          opacity: 0,
+          duration: 0.5,
+        } )
+        ;
+        gsap.from(".PSM_content", {
+          scrollTrigger: { 
+            trigger: ".PSM",
+            start: "+=60% +=100%",
+            end: "+=80% +=100%",
+            toggleActions: "restart none none reverse"
+          },
+          x: 400,
+          opacity: 0,
+          duration: 0.5,
+        });
+        gsap.to(".scene_container", {
+          scrollTrigger: {
+            trigger: ".hero_banner",
+            start: "+=35% +=30% ",
+            scrub: 0.3,
+            // toggleActions: "none restart none restart",
+          },
+          opacity: 0,
+          y: -500
+        });
+        gsap.to(".hero_cards, .hero_main_title", {
+          scrollTrigger: {
+            trigger: ".hero_banner",
+            start: "+=35% +=30% ",
+            end: "50%",
+            scrub: 0.3,
+            // toggleActions: "none restart none restart",
+          },
+          opacity: 0,
+          y: 100,
+        });
+        gsap.from(".desc_middle", {
+          scrollTrigger: {
+            trigger: ".desc",
+          start: "-35%  50%",
+          toggleActions: "restart none none reverse"
+        },
+        duration: .4,
+        scale: 3,
+        opacity: 0,
+        stagger: 0.3
+      })
+      gsap.from(".desc_left, .desc_right", {
+        scrollTrigger: {
+          trigger: ".desc",
+          start: "-25%  50%",
+          toggleActions: "restart none none reverse"
+        },
+        delay: 2,
+        duration: .2,
+        opacity: 0,
+        stagger: 0.3,
+        y : 100
+      })
+      
+    gsap.from(".Last_project_item", {
       scrollTrigger: {
-        trigger: ".hero_banner",
-        start: "+=35% +=30% ",
-        scrub: 0.3,
-        // toggleActions: "none restart none restart",
+          trigger: ".Last_project",
+    start: "-35%  50%",
+    toggleActions: "restart none none reverse"
       },
       opacity: 0,
-      y: -500
-    });
-    gsap.to(".hero_cards, .hero_main_title", {
-      scrollTrigger: {
-        trigger: ".hero_banner",
-        start: "+=35% +=30% ",
-        end: "50%",
-        scrub: 0.3,
-        // toggleActions: "none restart none restart",
-      },
-      opacity: 0,
-      y: 100,
-    });
-    gsap.from(".desc_middle", {
-      scrollTrigger: {
-        trigger: ".desc",
-        start: "-35%  50%",
-        toggleActions: "restart none none reverse"
-      },
-      duration: .4,
-      scale: 3,
-      opacity: 0,
+      x: 100,
       stagger: 0.3
-    })
-    gsap.from(".desc_left, .desc_right", {
+  })
+  gsap.from(".Last_project_name", {
       scrollTrigger: {
-        trigger: ".desc",
-        start: "-25%  50%",
-        toggleActions: "restart none none reverse"
+          trigger: ".Last_project",
+    start: "-35%  50%",
+    toggleActions: "restart none none reverse"
       },
-      delay: 2,
-      duration: .2,
       opacity: 0,
+      y: -200,
       stagger: 0.3,
-      y : 100
-    })
+  })
+    }
     gsap.from(".rolling_txt_right", {
       scrollTrigger: {
           trigger: '.PSM',
@@ -95,26 +153,7 @@ export default function Home() {
       },
       x: 1000
   })
-  gsap.from(".Last_project_item", {
-    scrollTrigger: {
-        trigger: ".Last_project",
-  start: "-35%  50%",
-  toggleActions: "restart none none reverse"
-    },
-    opacity: 0,
-    x: 100,
-    stagger: 0.3
-})
-gsap.from(".Last_project_name", {
-    scrollTrigger: {
-        trigger: ".Last_project",
-  start: "-35%  50%",
-  toggleActions: "restart none none reverse"
-    },
-    opacity: 0,
-    y: -200,
-    stagger: 0.3,
-})
+    
 
   }, []);
 
